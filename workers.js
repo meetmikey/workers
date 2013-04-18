62839185b2feb519c86c26c2079679b80e01896b
Justin Durack <justin@magicnotebook.com>
2013-04-18 11:12:55 -0700

    Added followLink job

--- workers.js
+++ workers.js
@@ -2,10 +2,11 @@ var serverCommon = process.env.SERVER_COMMON;
 
 var appInitUtils = require(serverCommon + '/lib/appInitUtils'),
     winston = require(serverCommon + '/lib/winstonWrapper').winston,
-    serverCommonConf = require (serverCommon + '/conf'),
-    imageUtils = require (serverCommon + '/lib/imageUtils'),
-    constants = require ('./constants'),
-    indexingHandler = require (serverCommon + '/lib/indexingHandler'),
+    serverCommonConf = require(serverCommon + '/conf'),
+    imageUtils = require(serverCommon + '/lib/imageUtils'),
+    followLinkUtils = require(serverCommon + '/lib/followLinkUtils'),
+    constants = require('./constants'),
+    indexingHandler = require(serverCommon + '/lib/indexingHandler'),
     sqsConnect = require(serverCommon + '/lib/sqsConnect');
 
 var initActions = [
@@ -16,11 +17,18 @@ var initActions = [
 
 appInitUtils.initApp( 'workers', initActions, serverCommonConf, function() {
 
+  var maxWorkers = constants.MAX_WORKER_JOBS;
+  if ( process && process.argv && ( process.argv.length > 2 ) ) {
+    maxWorkers = process.argv[2];
+  }
+
+  winston.doInfo('maxWorkers: ' + maxWorkers);
+
   sqsConnect.pollWorkerQueue(function (message, pollQueueCallback) {
 
     var job = JSON.parse (message);
 
-    if (job.jobType === 'thumbnail') {      
+    if (job.jobType === 'thumbnail') {
       imageUtils.doThumbnailingJob (job, function (err) {
         if (err) {
           pollQueueCallback (err);
@@ -40,11 +48,21 @@ appInitUtils.initApp( 'workers', initActions, serverCommonConf, function() {
         }
       });
     }
+    else if (job.jobType === 'followLink') {
+      followLinkUtils.doFollowLinkJob(job, function (err) {
+        if (err) {
+          pollQueueCallback (err);
+        }
+        else {
+          pollQueueCallback ();
+        }
+      });
+    }
     else {
       winston.doError ('Unsupported worker job on queue', {job : job});
       pollQueueCallback ();
     }
 
-  }, constants.MAX_WORKER_JOBS);
+  }, maxWorkers);
 
 });
