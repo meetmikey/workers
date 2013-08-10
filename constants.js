b69e417ca0776b8092a65e852bdf602be4817d2e
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-09 17:34:26 -0700

    add NODE_ENV constant

--- constants.js
+++ constants.js
@@ -15,6 +15,7 @@ var thumbnailer = process.env.IS_THUMBNAILER;
 define ('USE_REINDEXING_QUEUE', false);
 define ('MAX_WORKER_JOBS', 20);
 define ('CLOUD_ENV', cloudEnvironment);
+define ('NODE_ENV', environment);
 define ('MAX_INVALIDATION_JOBS', 10);
 define ('MAX_WORKER_THUMBNAIL_JOBS', 10);
 define ('THUMBNAILER', thumbnailer);
